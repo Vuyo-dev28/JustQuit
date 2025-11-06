d71dbb5800e2ee69c1acd91f1319a55c207773cf
iFlex Prod. <iflexprodbeats@gmail.com>
2025-11-06 16:45:30 +0000

    use this colour scheme:
    As the name implies, classic neutrals is a time

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,9 +9,7 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-      <div className="flex flex-col h-screen" style={{
-         backgroundImage: 'radial-gradient(circle, #0c1445 0%, #0c1445 40%, #000 100%)'
-      }}>
+      <div className="flex flex-col h-screen">
         <main className="flex-1 overflow-y-auto pb-28">{children}</main>
         <BottomNav />
       </div>
