db4034348ef62db8461c321bd9970f839a208b66
iFlex Prod. <iflexprodbeats@gmail.com>
2025-11-12 13:13:43 +0000

    prepare this so i can export a .ipa file using capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.vuyom.justquit',
   appName: 'JustQuit',
-  webDir: '.',
+  webDir: 'out',
  
 };
 
